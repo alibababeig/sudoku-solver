ff7f2978fe8c65ca5a9436f533a62a9a1bcdad59
alibababeig <alibababeig@yahoo.com>
2023-04-18 02:52:52 +0330

    Added class InputManager

---
+++ utils/input.py
@@ -0,0 +1,24 @@
+class InputManager:
+    def __init__(self):
+        self._numbers = [f'{x}' for x in range(1, 10)]
+        self._empty_sign = '-'
+
+    def load(self, file_path):
+        with open(file_path, 'r') as f:
+            matrix = []
+            for i in range(9):
+                row = f.readline().split()
+
+                if len(row) != 9:
+                    raise ValueError()
+
+                for i, x in enumerate(row):
+                    if x in self._numbers:
+                        row[i] = int(x)
+                    elif x == self._empty_sign:
+                        row[i] = 0
+                    else:
+                        raise ValueError()
+
+                matrix.append(row)
+        return matrix
